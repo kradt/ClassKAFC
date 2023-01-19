69d85be1d38c4911150ad947151eadfa527d09c7
dark <bigmag_@MacBook-Pro-Dark.local>
2023-01-19 18:45:41 +0200

    add runtime

--- kafc/config.py
+++ kafc/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 	DEBUG = True
 	SECRET_KEY = os.getenv("SECRET_KEY")
-	SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+	SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 	BUCKET_NAME = os.getenv("BUCKET_NAME")
 	AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 	AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
