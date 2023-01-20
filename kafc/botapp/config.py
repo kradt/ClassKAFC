776750b0f0c6a75899685b92e90286b6d349636e
dark <bigmag_@MacBook-Pro-Dark.local>
2023-01-20 14:53:46 +0200

    add runtime

--- kafc/botapp/config.py
+++ kafc/botapp/config.py
@@ -2,7 +2,7 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URI = os.getenv("DATABASE_URL")
-WEBHOOK_HOST = '1ba3-178-133-216-61.eu.ngrok.io'
+WEBHOOK_HOST = 'prtf.online'
 WEBHOOK_PORT = 443
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/webhook/%s/" % (BOT_TOKEN)
