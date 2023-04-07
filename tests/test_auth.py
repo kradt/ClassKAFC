6cb151f6fcdc2afd85684cd14151b89a77d2d2d8
krad <mynamedark713@gmail.com>
2023-04-08 00:18:41 +0300

    remove test register

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,14 +7,4 @@ def test_request_example(client):
     assert b"Please log in to access this page" in response.data
 
 
-def test_registration(client, app):
-    response = client.post(
-        "/auth/sign-up",
-        data={"login": "test@test.com", "password": "A9090997a", "repeat_password": "A9090997a"},
-        follow_redirects=True)
 
-    print(response)
-    assert response.status_code == 200
-    with app.app_context():
-        assert models.User.query.count() == 1
-        assert models.User.query.first().username == "test@test.com"
