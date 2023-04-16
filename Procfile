d2e924c9848d0aad71f732d4076bf1561f9c64b9
krad <mynamedark713@gmail.com>
2023-04-16 13:53:57 +0300

    some refactoring

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn "kafc:create_app()" -b 0.0.0.0:$PORT
--- wsgi.py
+++
@@ -1,6 +0,0 @@
-from kafc import create_app
-
-
-if __name__ == '__main__':
-	app = create_app()
-	app.run(port=80, host="0.0.0.0",)
