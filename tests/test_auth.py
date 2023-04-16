f01772aeef91d5724f990cfb9f9a7e51a2564be5
krad <mynamedark713@gmail.com>
2023-04-16 13:08:04 +0300

    add test_update_user_information

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -44,7 +44,9 @@ def test_cabinet_page_with_login(client, login):
     assert response.status_code == 200
     assert bytes("Введіть Ім'я яке студенти будуть бачити при відправленні завдання", "utf-8") in response.data
 
-def test_send_task_with_login(client, login, lesson):
+
+def test_send_task_with_login(app, client, login, lesson):
+
     response = client.post("/me/send-task",
                            data={"title": "Hello world",
                                  "description": "Bye world",
@@ -54,3 +56,13 @@ def test_send_task_with_login(client, login, lesson):
     print(response)
     assert response.request.full_path == "/me/?from_task=True"
     assert response.status_code == 200
+
+
+def test_update_user_information(client, login):
+    name = "Alex"
+    lesson = "OOP"
+    response = client.post("/me/update_user", data={"name": name, "lesson": lesson}, follow_redirects=True)
+    print(response)
+    assert response.status_code == 200
+    assert bytes(name, "utf-8") in response.data
+    assert bytes(lesson, "utf-8") in response.data
